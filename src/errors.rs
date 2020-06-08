0a58cd0edc67c81694b829186f9a7333ad383a72
jlgerber <jlgerber@gmail.com>
2020-06-07 21:30:19 -0700

    adds error variants to AmuriError

--- src/errors.rs
+++ src/errors.rs
@@ -1,7 +1,16 @@
-use thiserror::Error;
-
+use strum;
+use thiserror::Error; //ParseError
 #[derive(Error, Debug, PartialEq, Eq)]
 pub enum AmuriError {
     #[error("Unable to convert str {target} to int")]
     StrToIntError { target: String },
+    #[error("Error parsing uri {cause:?}")]
+    UriParsingError { cause: String },
+    #[error("Error parsing scheme from str {cause:?}")]
+    SchemeParsingError {
+        #[from]
+        cause: strum::ParseError,
+    },
+    #[error("Failed to parse level from {0}")]
+    LevelParsingFailure(String),
 }
